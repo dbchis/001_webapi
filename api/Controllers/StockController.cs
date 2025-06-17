c4284c80749533c75e3bdef9fbc3f9cc02863bf1
chidb2k3 <dangbachi345@gmail.com>
2025-06-17 20:40:41 +0700

    17_06

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _001_webapi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _001_webapi.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stock = _context.Stocks.ToList();
+            return Ok(stock);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
